1d893c21d922688ca7b7b8cfffa8e4d6c05a6a21
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-10-22 10:44:40 +0300

    Test was modified

--- src/test/java/ru/netology/test/AppTesting.java
+++ src/test/java/ru/netology/test/AppTesting.java
@@ -31,43 +31,7 @@ public class AppTesting {
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
     }
-   /* @Test
-    void test1() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:8080");
-        //$x("//*[@class='button button_size_m button_theme_alfa-on-white']").click();
-        $x("//*[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $x("//input[@placeholder='0000 0000 0000 0000']").click();
-        $x("//input[@placeholder='0000 0000 0000 0000']").setValue("4444 4444 4444 4442");
-        $x("//input[@placeholder='08']").click();
-        $x("//input[@placeholder='08']").setValue("10");
-        $x("//input[@placeholder='22']").click();
-        $x("//input[@placeholder='22']").setValue("22");
-        $x("//*[@class='form form_size_m form_theme_alfa-on-white']//div[last()-1]/span/span/span/span/span/input").click();
-        $x("//*[@class='form form_size_m form_theme_alfa-on-white']//div[last()-1]/span/span/span/span/span/input").setValue("Alex Navalniy");
-        $x("//input[@placeholder='999']").click();
-        $x("//input[@placeholder='999']").setValue("645");
-        $x("//*[@class='form form_size_m form_theme_alfa-on-white']//div[last()]/button").click();
 
-
-    }
-
-    @Test
-    void test2() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:8080");
-        $x("//*[@class='button button_size_m button_theme_alfa-on-white']").click();
-        $x("//*[@class='form form_size_m form_theme_alfa-on-white']//div[last()-1]/span/span/span/span/span/input").click();
-        $x("//*[@class='form form_size_m form_theme_alfa-on-white']//div[last()-1]/span/span/span/span/span/input").setValue(Data.generateName("en"));
-        $x("//input[@placeholder='999']").click();
-        $x("//input[@placeholder='999']").setValue(Data.generateCVV());
-        $x("//input[@placeholder='08']").click();
-        $x("//input[@placeholder='08']").setValue(Data.generateMonth(6));
-        $x("//input[@placeholder='22']").click();
-        $x("//input[@placeholder='22']").setValue(Data.generateYear(3));
-        $x("//input[@placeholder='0000 0000 0000 0000']").click();
-        $x("//input[@placeholder='0000 0000 0000 0000']").setValue(Data.getCardNumberSecond());
-    }*/
 
     @Test
     void test1() {
